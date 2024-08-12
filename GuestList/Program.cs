32cbc04e71c72d64adbb4b86b7ca0cb2cc2144e8
Ahmet Sulu <ahmet.sulu1993@gmail.com>
2024-08-12 16:49:10 +0300

    Add project files.

---
+++ GuestList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Ziyaretçilerin isimlerini tutan bir liste oluşturuyoruz.
+        List<string> guestNames = new List<string>
+        {
+            "Bülent Ersoy",
+            "Ajda Pekkan",
+            "Ebru Gündeş",
+            "Hadise",
+            "Hande Yener",
+            "Tarkan",
+            "Funda Arar",
+            "Demet Akalın"
+        };
+
+        // Listeyi ekrana yazdırıyoruz.
+        Console.WriteLine("Gala gecesine davetli ziyaretçiler:");
+        foreach (string guestName in guestNames)
+        {
+            Console.WriteLine(guestName);
+        }
+    }
+}
